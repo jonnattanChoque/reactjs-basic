dbcf83c6416a4a0da81e532960c076d60b099d75
Jhonnatan Choque <JonnatanChoque@Jhonnatans-MacBook-Pro.local>
2021-01-18 19:13:24 -0500

    componentes y props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Hola from "./component/Hola";
+import Number from "./component/Number";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<Hola title="desde react js" array={[2, 3, 4]} />
+				<Number text="Mi número es" num={1234} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/component/Number.js
@@ -0,0 +1,9 @@
+function Number({text, num}) {
+	return (
+		<div className="App">
+			<p>{text} {num}</p>
+		</div>
+	);
+}
+
+export default Number;
